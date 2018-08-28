172e6ff0d3e2624cd9a5023d44c7e065b9ef9d2c
Marksman111 <wlb825077462@gmail.com>
2018-08-28 21:22:28 +0800

    test

--- springboot07-test/src/test/java/com/marksman/HelloControllerTests.java
+++ springboot07-test/src/test/java/com/marksman/HelloControllerTests.java
@@ -6,8 +6,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
@@ -17,6 +23,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class HelloControllerTests {
 
     @Autowired
@@ -28,7 +35,12 @@ public class HelloControllerTests {
    }
 
    @Test
-   public void getHello(){
-
+   public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("hello")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
    }
+
 }
