a10d50b3474f28102305ff106237393bdc3a2b43
Marksman111 <wlb825077462@gmail.com>
2018-08-30 17:59:21 +0800

    springboot-actuator

---
+++ springboot12-actuator/src/main/java/com/marksman/springboot12actuator/Springboot12ActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.marksman.springboot12actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot12ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot12ActuatorApplication.class, args);
+    }
+}
